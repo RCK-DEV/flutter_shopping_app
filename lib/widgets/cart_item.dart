f8752a5a7abe6d85f2cc259d8f5520858ae61cfe
Rick Korkmaz <rickkorkmaz@gmail.com>
2022-03-17 23:36:54 +0100

    feat: confirm remove cart item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -20,6 +20,32 @@ class CartItem extends StatelessWidget {
       onDismissed: (directiom) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (dismissDirection) {
+        return showDialog(
+          context: context,
+          builder: (ctx) {
+            return AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('Do you want to remove the item from your cart?'),
+              actions: [
+                FlatButton(
+                  child: Text('Yes'),
+                  onPressed: () {
+                    Navigator.of(ctx).pop(true);
+                  },
+                ),
+                FlatButton(
+                  child: Text('No'),
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                ),
+              ],
+            );
+          },
+        );
+        return null;
+      },
       background: Container(
         color: Theme.of(context).errorColor,
         alignment: Alignment.centerRight,
