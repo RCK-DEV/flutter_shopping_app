7ce0badc4ecc801102c2d85b10e487057611ff57
Rick Korkmaz <rickkorkmaz@gmail.com>
2022-03-22 14:13:04 +0100

    Pull-to-refresh user products screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:io' show Platform;
 
 import '../providers/product.dart';
 import '../providers/products.dart';
@@ -9,6 +11,10 @@ import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +32,32 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: SingleChildScrollView(
-          child: Column(
-              children: products.map((product) {
-            return UserProductItem(product.id, product.title, product.imageUrl);
-          }).toList()),
-        ),
-      ),
+      body: Platform.isAndroid
+          ? RefreshIndicator(
+              onRefresh: () => _refreshProducts(context),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: SingleChildScrollView(
+                  child: Column(
+                      children: products.map((product) {
+                    return UserProductItem(product.id, product.title, product.imageUrl);
+                  }).toList()),
+                ),
+              ),
+            )
+          : Padding(
+              padding: EdgeInsets.all(8),
+              child: CustomScrollView(
+                slivers: [
+                  CupertinoSliverRefreshControl(onRefresh: () => _refreshProducts(context)),
+                  SliverList(
+                      delegate: SliverChildBuilderDelegate(((context, index) {
+                    return UserProductItem(
+                        products[index].id, products[index].title, products[index].imageUrl);
+                  }), childCount: products.length))
+                ],
+              ),
+            ),
     );
   }
 }
